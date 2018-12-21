26aa1f20033773d24706eb37716ade0afcc1b780
Hugo Pires <hugo.pires@payulatam.com>
2018-12-21 14:25:18 -0200

    testing

--- src/main/test/java/com/hugoltsp/spring/boot/starter/jwt/filter/test/JwtAuthenticationFilterTest.java
+++ src/main/test/java/com/hugoltsp/spring/boot/starter/jwt/filter/test/JwtAuthenticationFilterTest.java
@@ -14,9 +14,15 @@ import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import java.util.Optional;
+
 import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpMethod.GET;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JwtAuthenticationFilterTest {
@@ -37,7 +43,7 @@ public class JwtAuthenticationFilterTest {
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Test
-    public void should_return_ok_when_request_is_deemed_public() throws Exception {
+    public void should_return_200_when_request_is_deemed_public() throws Exception {
 
         MockHttpServletRequest request = new MockHttpServletRequest();
 
@@ -49,6 +55,41 @@ public class JwtAuthenticationFilterTest {
         jwtAuthenticationFilter.doFilter(request, response, filterChain);
 
         assertThat(response.getStatus()).isEqualTo(SC_OK);
+    }
+
+    @Test
+    public void should_return_200_when_request_is_protected_and_user_is_authenticated() throws Exception {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod(GET.name());
+        request.setRequestURI("/test");
+        request.addHeader(AUTHORIZATION, "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdWIiLCJuYW1lIjoiSm9obiBEb2UifQ.jKexo6MlFW78w31biGfZGqaf3LRY3KZKMuXJFtkCJ6k");
+
+        when(settings.isPublic(request)).thenReturn(false);
+        when(settings.getSecretKey()).thenReturn("some-secret-key");
+        when(userDetailsFinder.findByClaims(any())).thenReturn(Optional.empty());
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockFilterChain filterChain = new MockFilterChain();
+
+        jwtAuthenticationFilter.doFilter(request, response, filterChain);
+
+        assertThat(response.getStatus()).isEqualTo(SC_OK);
+    }
+
+    @Test
+    public void should_return_401_when_request_is_protected() throws Exception {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        when(settings.isPublic(request)).thenReturn(false);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockFilterChain filterChain = new MockFilterChain();
+
+        jwtAuthenticationFilter.doFilter(request, response, filterChain);
+
+        assertThat(response.getStatus()).isEqualTo(SC_UNAUTHORIZED);
     }
 
 }
